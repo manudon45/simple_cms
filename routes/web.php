015c8be61d57e51185daa15786f85ce14b747dab
Manish Ray <raymanish829@gmail.com>
2026-08-12 00:07:30 +1200

    Lab 3 Part 3 completed

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,26 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'posts.index')->name('home');
 
-Route::get('/register', function () {
-    return view('auth.register');
-})->name('register');
+// Route::get('/register', function () {
+//     return view('auth.register');
+// })->name('register');
 
-Route::post('/register',[AuthController::class,'register']);
+// Route::post('/register',[AuthController::class,'register']);
 
-Route::view('/login', 'auth.login')->name('login');
-Route::post('/login',[AuthController::class,'login']);
+// Route::view('/login', 'auth.login')->name('login');
+// Route::post('/login',[AuthController::class,'login']);
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-Route::post('/logout', [AuthController::class,'logout'])->name('logout');
+// Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+// Route::post('/logout', [AuthController::class,'logout'])->name('logout');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [DashboardController::class,'index'])->middleware('auth')->name('dashboard');
+    Route::post('/logout', [AuthController::class,'logout'])->name('logout');
+});
+
+Route::middleware('guest')->group(function () {
+    Route::view('/register', 'auth.register')->name('register');
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::view('/login', 'auth.register')->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+});
