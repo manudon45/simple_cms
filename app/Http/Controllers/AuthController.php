440a43589da1b4c804b30387c9e768eac9f3e618
manudon45 <raymanish829@gmail.com>
2026-08-11 10:21:30 +0545

    Lab 2

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) {
+        $fields = $request->validate([
+            'username' => ['required', 'max:255'],
+            'password'=> ['required','min:3', 'confirmed'],
+            'email' => ['required', 'email','max:255', 'unique:users'],
+        ]);
+
+        $user = User::create($fields);
+        Auth::login($user);
+        return redirect()->route('home');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'posts.index')->name('home');
+
+Route::get('/register', function () {
+    return view('auth.register');
+})->name('register');
+
+Route::post('/register',[AuthController::class,'register']);
