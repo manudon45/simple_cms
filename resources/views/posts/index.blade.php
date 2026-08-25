f5374e589a8a5eb49c4cdffe40b1944cc2635d06
Manish Ray <raymanish829@gmail.com>
2026-08-26 03:46:25 +1200

    Week 4 Lab 2 Completed

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,3 +1,23 @@
 <x-layout>
-   <h1 class="title">Latest Posts</h1>
+    <h1 class="title">Latest Post</h1>
+    <div class="grid grid-cols-2 gap-6">
+        @foreach ($posts as $post)
+            <div class="card">
+                {{-- title --}}
+                <h2 class="font-bold text-xl">{{$post->title}}</h2>
+                {{-- author and date --}}
+                <div class="text-xs">
+                    <span>Posted {{$post->created_at->diffForHumans()}} by</span>
+                    <a href="" class="text-blue-500 font-medium">Username</a>
+                </div>
+                {{-- body --}}
+                <div class="text-sm">
+                    <p>{{Str::words($post->body, 15)}}</p>
+                </div>
+            </div>
+        @endforeach
+    </div>
+    <div>
+        {{$posts->links()}}
+    </div>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::middleware('auth')->group(function () {
 Route::middleware('guest')->group(function () {
     Route::view('/register', 'auth.register')->name('register');
     Route::post('/register', [AuthController::class, 'register']);
-    Route::view('/login', 'auth.register')->name('login');
+    Route::view('/login', 'auth.login')->name('login');
     Route::post('/login', [AuthController::class, 'login']);
 });
 
