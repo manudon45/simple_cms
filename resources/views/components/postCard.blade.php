eb07b5a5ba7d804b9df67b83b22aad894a8e1267
Manish Ray <raymanish829@gmail.com>
2026-09-06 18:10:24 +1200

    update

--- resources/views/components/postCard.blade.php
+++ resources/views/components/postCard.blade.php
@@ -1,14 +1,21 @@
-@props(['post'])
+@props(['post', 'full' => false])
 <div class="card">
     {{-- title --}}
     <h2 class="font-bold text-xl">{{$post->title}}</h2>
     {{-- author and date --}}
     <div class="text-xs">
         <span>Posted {{$post->created_at->diffForHumans()}} by</span>
-        <a href="{{ route('posts.user', $post->user) }}" class="text-blue-500 font-medium">{{ $post->user->username }}</a>
+        <a href="{{route('posts.user', $post->user)}}" class="text-blue-500 font-medium">{{$post->user->username}}</a>
     </div>
     {{-- body --}}
-    <div class="text-sm">
-        <p>{{Str::words($post->body, 15)}}</p>
-    </div>
+    @if ($full)
+        <div class="text-sm">
+            <span>{{$post->body}}</span>
+        </div>
+    @else
+        <div class="text-sm">
+            <span>{{Str::words($post->body, 15)}}</span>
+            <a href="{{route('posts.show', $post)}}" class="text-blue-500">Read more &rarr;</a>
+        </div>
+    @endif
 </div>
